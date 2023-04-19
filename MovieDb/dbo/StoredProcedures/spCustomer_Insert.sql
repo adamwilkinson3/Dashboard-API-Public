54a0d0a1cfeb277e3061728a2672ad0b90a3c735
adamwilkinson3 <82132359+adamwilkinson3@users.noreply.github.com>
2023-04-18 21:26:07 -0400

    Added DB Files

---
+++ MovieDb/dbo/StoredProcedures/spCustomer_Insert.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spCustomer_Insert]
+	@first_name nvarchar(45),
+	@last_name nvarchar(45),
+	@email nvarchar(50),
+	@active bit,
+	@create_date datetime
+AS
+begin
+	insert into [dbo].[customer] (first_name, last_name, email, active, create_date)
+	values (@first_name, @last_name, @email, @active, @create_date);
+end
---
+++ MovieDb/dbo/Tables/customer.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[customer]
+(
+	[id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[first_name] NVARCHAR(45),
+	[last_name] NVARCHAR(45),
+	[email] NVARCHAR(50),
+	[active] BIT,
+	[create_date] DATETIME NOT NULL,
+	[last_update] TIMESTAMP
+)
